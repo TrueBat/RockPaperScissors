e6a027cb6b70eaf7c44d6dbcde5fb6bf527c9cb8
TrueBat <b4tb0ywalker@gmail.com>
2022-07-02 23:26:40 +0300

    5 rounds game

--- RPS.js
+++ RPS.js
@@ -38,17 +38,44 @@ scissors.addEventListener('click' , function(){
 });
 
 //game logic:
+var playerScore = 0;
+var botScore = 0;
+var gamesPlayed = 0;
+var winner = "";
+
+let test = "fak";
 
 function game(playerChoice) {
-    let botChoice = botPlay();
-    let pc = playerChoice.toLowerCase();
-    let bc = botChoice.toLowerCase();
-    if (pc === bc) {
-        return "its a draw the bot chose " + botChoice;
-    }else if (pc === "rock" && bc === "paper" || pc === "paper" && bc === "scissors" || pc === "scissors" && bc === "rock") {
-        return "you lose the bot chose " + botChoice;
-    }else if (pc === "rock" && bc === "scissors" || pc === "paper" && bc === "rock" || pc === "scissors" && bc === "paper") {
-        return "you win the bot chose " + botChoice;
-    }
+
+        let botChoice = botPlay();
+        let pc = playerChoice.toLowerCase();
+        let bc = botChoice.toLowerCase();
+        let message = "";
+
+        if (pc === bc) {
+            message = "its a draw the bot chose " + botChoice;
+        }else if (pc === "rock" && bc === "paper" || pc === "paper" && bc === "scissors" || pc === "scissors" && bc === "rock") {
+            botScore++;
+            gamesPlayed++;
+            message = "you lose the bot chose " + botChoice;
+        }else if (pc === "rock" && bc === "scissors" || pc === "paper" && bc === "rock" || pc === "scissors" && bc === "paper") {
+            playerScore++;
+            gamesPlayed++;
+            message = "you win the bot chose " + botChoice;
+        }
     
+        if (gamesPlayed >= 5) {
+
+            playerScore = 0;
+            botScore = 0;
+            gamesPlayed = 0;
+            const gameEnd = "5 games ended, ";
+    
+            if (playerScore > botScore) {
+                message += " " + gameEnd + "and you won!";
+            } else{
+                message += " " + gameEnd + "and the bot won!";
+            }
+        }
+        return message;
 }
